acbbb302cbcc94cac516c7054c0a9df9e6d987d1
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2018-04-09 19:02:02 +0200

    font

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/layouts/fonts`,
+        path: `${__dirname}/static/fonts`,
         name: 'fonts',
       },
     },       
