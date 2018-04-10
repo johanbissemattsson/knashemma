a6808e36180c9afca281fbfd56beaaf62e5b7880
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2018-04-10 10:51:23 +0200

    Remove styled components from index

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { injectGlobal } from "styled-components";
-import CalibreWebRegularEOT from "./fonts/CalibreWeb-Regular.eot";
-import CalibreWebRegularWOFF from "./fonts/CalibreWeb-Regular.woff";
-import CalibreWebRegularWOFF2 from "./fonts/CalibreWeb-Regular.woff2";
-import CalibreWebSemiboldEOT from "./fonts/CalibreWeb-Semibold.eot";
-import CalibreWebSemiboldWOFF from "./fonts/CalibreWeb-Semibold.woff";
-import CalibreWebSemiboldWOFF2 from "./fonts/CalibreWeb-Semibold.woff2";
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -16,16 +9,6 @@ import './index.css';
 import 'hamburgers/dist/hamburgers.css';
 import 'slick-carousel/slick/slick.css';
 
-injectGlobal`
-  @font-face {
-    font-family: 'Calibre Regular';
-    font-style: normal;
-    font-weight: 300;
-    src: url(${CalibreWebRegularEOT});
-    src: url(${CalibreWebRegularWOFF2}) format('woff2'),
-      url(${CalibreWebRegularWOFF}) format('woff'),
-  }
-`
 
 export default class TemplateWrapper extends Component {
   render() {
